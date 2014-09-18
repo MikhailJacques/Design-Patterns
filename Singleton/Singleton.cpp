014afb919cb46ef11ddb0f93713aee17a46ad127
Mikhail Jacques <jacques.mikhail@gmail.com>
2014-09-18 21:31:54 +0300

    update project Singleton

--- Singleton/Singleton.cpp
+++ Singleton/Singleton.cpp
@@ -22,17 +22,18 @@ class Singleton
 {
 	public:
 
-		static Singleton* getInstance();
-		~Singleton();
+		static Singleton * getInstance();
+		//~Singleton();
 		void name();
 		inline int getValue() { return m_value; }
 		inline void setValue( int v ) { m_value = v; }
 		void printValue();
+		void release();
 
 	private:
 
-		static Singleton *single;
 		int m_value;
+		static Singleton * m_singleton;
 
 		// Declare these three private. This ensures they are unaccessable 
 		// so that we do not accidently get copies of our singleton appearing
@@ -42,34 +43,86 @@ class Singleton
 };
 
 // Global static pointer used to ensure a single instance of the class.
-Singleton* Singleton::single = NULL;
+Singleton* Singleton::m_singleton = NULL;
 
 
 // This function is called to create an instance of the class.
 Singleton* Singleton::getInstance()
 {
-	if ( !single )
-		single = new Singleton();
+	if ( !m_singleton )
+		m_singleton = new Singleton();
 
-	return single;
-}
+	return m_singleton;
 
-Singleton::~Singleton()
-{
-	if ( single )
-		delete single;
+	// In single-threaded applications a locally static instance can be used instead of a dynamically allocated instance.
+	// static Singleton m_singleton;
+    // return &m_singleton;
 }
 
 void Singleton::name()
 {
-	cout << "My name is Singleton" << endl;
-	cout << "My address is: " << this << endl;
+	cout << "My name is Singleton\nMy address is: " << this << endl;
 }
 
 void Singleton::printValue() 
 { 
 	cout << "My value is: " << m_value << endl; 
 }
+
+void Singleton::release()
+{
+	cout << "\nSingleton has been destroyed\n";
+}
+
+// Do not be tempted to use the destructor to destroy the Singleton object.
+//Singleton::~Singleton()
+//{
+//	// calling 'delete m_singleton' will invoke the destructor, 
+//	// which in turn will try to delete _instance once more, infinitely. 
+//	if ( m_singleton )
+//		delete m_singleton;
+//}
+
+
+// This implementation is known as Scott Mayers' Singleton
+// This approach is founded on C++'s guarantee that local static objects are intiialized 
+// when the object's definition is first encountered during a call to that function.
+// As a bonus, if you never call a function emulating a non-local static object, 
+// you never incur the cost of constructing and destructing the object.
+
+// When we call Singleton& s = Singleton::Instance() first time the object is created 
+// and every next call to Singleton::Instance() results with the same object being returned. 
+// Main issue: subject to 'Destruction Order Fiasco'
+class Singleton2
+{
+	public:
+
+		static Singleton2 & getInstance() 
+		{
+			static Singleton2 m_singleton;
+			return m_singleton;
+		}
+
+		void name()
+		{
+			cout << "My name is Singleton 2\nMy address is: " << this << endl;
+		}
+
+		int getValue() { return m_value; }
+		void setValue( int v ) { m_value = v; }
+		void printValue() { cout << "My value is: " << m_value << endl; }
+
+	private:
+
+		int m_value;
+
+		Singleton2() { };
+		~Singleton2() { };
+		Singleton2( Singleton2 const & );
+		Singleton2 & operator=( Singleton2 const & );
+};
+
+
 
 int main()
 {
@@ -86,6 +139,22 @@ int main()
 	sc2->name();
 	sc1->setValue(7);
 	sc2->printValue();
+
+	sc1->release();
+
+	cout << endl;
+
+	Singleton2 & sc3 = Singleton2::getInstance();
+	sc3.name();
+	sc3.setValue(3);
+	sc3.printValue();
+
+	cout << endl;
+
+	Singleton2 & sc4 = Singleton2::getInstance();
+	sc4.name();
+	sc3.setValue(5);
+	sc4.printValue();
 
 	cout << endl;
 
