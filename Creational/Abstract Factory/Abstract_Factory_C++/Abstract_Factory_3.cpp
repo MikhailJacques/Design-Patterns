bbfa61d498ff99d33c30a1ae0e93641d38c980e8
Mikhail Jacques <jacques.mikhail@gmail.com>
2014-12-14 13:33:37 +0200

    add C++ Abstract Factory files

--- Creational/Abstract Factory/Abstract_Factory_C++/Abstract_Factory_3.cpp
+++ Creational/Abstract Factory/Abstract_Factory_C++/Abstract_Factory_3.cpp
@@ -191,38 +191,49 @@ PhoneFactory * PhoneFactory::CreateFactory(VENDORS factory)
 
 int main(int argc, char* argv[])
 {
+	IDumb * dumb_phone = NULL;
+	ISmart * smart_phone = NULL;
 	PhoneFactory * factory = NULL;
-	// Note that the use of smart pointers will get rid of the delete statements below
+
+	// Note that the use of smart pointers will get rid of the delete statements below.
 	// The issue that smart pointers do not permit instantiation of abstract classes.
 	// unique_ptr<PhoneFactory> factory = NULL;
 	// shared_ptr<PhoneFactory> factory = NULL;
 
 	factory = PhoneFactory::CreateFactory(PhoneFactory::VENDORS::SAMSUNG);
-	cout << "Dumb phone from Samsung: " << factory->GetDumb()->Name() << "\n"; 
-	cout << "Smart phone from Samsung: " << factory->GetSmart()->Name() << "\n\n";
-	delete factory->GetDumb();
-	delete factory->GetSmart();
+	dumb_phone = factory->GetDumb();
+	smart_phone = factory->GetSmart();
+	cout << "Dumb phone from Samsung: " << dumb_phone->Name() << "\n"; 
+	cout << "Smart phone from Samsung: " << smart_phone->Name() << "\n\n";
+	delete dumb_phone;
+	delete smart_phone;
 	delete factory;
 
 	factory = PhoneFactory::CreateFactory(PhoneFactory::VENDORS::HTC);
-	cout << "Dumb phone from HTC: " << factory->GetDumb()->Name() << "\n";
-	cout << "Smart phone from HTC: " << factory->GetSmart()->Name() << "\n\n";
-	delete factory->GetDumb();
-	delete factory->GetSmart();
+	dumb_phone = factory->GetDumb();
+	smart_phone = factory->GetSmart();
+	cout << "Dumb phone from HTC: " << dumb_phone->Name() << "\n";
+	cout << "Smart phone from HTC: " << smart_phone->Name() << "\n\n";
+	delete dumb_phone;
+	delete smart_phone;
 	delete factory;
 	
 	factory = PhoneFactory::CreateFactory(PhoneFactory::VENDORS::NOKIA);
-	cout << "Dumb phone from Nokia: " << factory->GetDumb()->Name() << "\n";
-	cout << "Smart phone from Nokia: " << factory->GetSmart()->Name() << "\n\n";	
-	delete factory->GetDumb();
-	delete factory->GetSmart();
+	dumb_phone = factory->GetDumb();
+	smart_phone = factory->GetSmart();
+	cout << "Dumb phone from Nokia: " << dumb_phone->Name() << "\n";
+	cout << "Smart phone from Nokia: " << smart_phone->Name() << "\n\n";	
+	delete dumb_phone;
+	delete smart_phone;
 	delete factory;
 
 	factory = PhoneFactory::CreateFactory(PhoneFactory::VENDORS::APPLE);
-	cout << "Dumb phone from Apple: " << factory->GetDumb()->Name() << "\n";
-	cout << "Smart phone from Apple: " << factory->GetSmart()->Name() << "\n\n";	
-	delete factory->GetDumb();
-	delete factory->GetSmart();
+	dumb_phone = factory->GetDumb();
+	smart_phone = factory->GetSmart();
+	cout << "Dumb phone from Apple: " << dumb_phone->Name() << "\n";
+	cout << "Smart phone from Apple: " << smart_phone->Name() << "\n\n";	
+	delete dumb_phone;
+	delete smart_phone;
 	delete factory;
 
 	cin.get(); // 	getchar();
